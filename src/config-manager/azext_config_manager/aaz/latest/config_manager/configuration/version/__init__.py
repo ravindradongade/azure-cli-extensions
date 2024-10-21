a30f2adce2b839e722d131b7ba918714497aa5fc
Ravindra Dongade <rdongade@microsoft.com>
2024-10-21 13:58:53 +0530

    updated clis

--- src/config-manager/azext_config_manager/aaz/latest/config_manager/configuration/version/__init__.py
+++ src/config-manager/azext_config_manager/aaz/latest/config_manager/configuration/version/__init__.py
@@ -9,5 +9,8 @@
 # flake8: noqa
 
 from .__cmd_group import *
+
 from ._list import *
 from ._show import *
+
+from ._wait import *
--- src/config-manager/azext_config_manager/aaz/latest/config_manager/hierarchy_list/__cmd_group.py
+++ src/config-manager/azext_config_manager/aaz/latest/config_manager/hierarchy_list/__cmd_group.py
@@ -12,7 +12,7 @@ from azure.cli.core.aaz import *
 
 
 @register_command_group(
-    "config-manager hierarchy-list",
+    "config-manager hierarchies",
     is_preview=True,
 )
 class __CMDGroup(AAZCommandGroup):
